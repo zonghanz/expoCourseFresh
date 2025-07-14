d4bfd6f464fd06b3c2160a9a42acbc43c5fd58c8
zonghanz <132874148+zonghanz@users.noreply.github.com>
2025-07-14 14:39:58 +0800

    Initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,13 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
+import { Stack } from "expo-router";
+import { Text } from "react-native";
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <>
+      <Text>Hello</Text>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="index" options={{title: "Home"}}/>
+        <Stack.Screen name="login" options={{title: "Login"}}/>
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    </>
+    );
 }
---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.view}
+    >
+      <Text>Breuhh</Text>
+      <Link href="/login" style={styles.navButton}>Login Page</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  view: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+  },
+  navButton: {
+    width:230, height:50, backgroundColor:"coral", borderRadius: 8, textAlign: "center"
+  }
+}) //23:26
